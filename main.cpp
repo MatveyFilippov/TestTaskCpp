116315d0f3f43ae1d95ce372914cb7c21336abde
MF <matdufi2004@gmail.com>
2024-02-03 11:39:20 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//  Created by Матвей Филиппов 22БИ-2 (2024-02-03).
+//  Вариант 1
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
